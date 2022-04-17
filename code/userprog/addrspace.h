d47c0fd2093c3f2d60fe2866d4073414df60be56
TongYiheng <541586028@qq.com>
2022-04-17 16:26:45 +0800

    finish lab6

--- code/userprog/addrspace.h
+++ code/userprog/addrspace.h
@@ -31,6 +31,8 @@ class AddrSpace {
     void SaveState();			// Save/restore address space-specific
     void RestoreState();		// info on a context switch 
 
+    void Print();
+
   private:
     TranslationEntry *pageTable;	// Assume linear page table translation
 					// for now!
--- code/userprog/progtest.cc
+++ code/userprog/progtest.cc
@@ -32,7 +32,7 @@ StartProcess(char *filename)
     }
     space = new AddrSpace(executable);    
     currentThread->space = space;
-
+    space->Print();
     delete executable;			// close file
 
     space->InitRegisters();		// set the initial register values
